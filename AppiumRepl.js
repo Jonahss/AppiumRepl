ec9109df737d62058b3aba5108dbb4f8bcd65adb
Moiz Virani <moizjv@gmail.com>
2014-08-01 14:55:33 -0700

    Adding package.json and removing node_modules

--- AppiumRepl.js
+++ AppiumRepl.js
@@ -1,16 +1,19 @@
-var wd = require('wd')
-var repl = require('repl')
-var color = require('colors')
-var telme = require('telme-promise')
+var wd = require('wd');
+var repl = require('repl');
+var color = require('colors');
+var telme = require('telme-promise');
+var prompt = require('prompt');
+var _      = require('underscore');
 
-var profiles = require('./caps.json')
+var profiles = require('./caps.json');
+var profile = null;
 
 var sauce = {
   hostname: "localhost",
   port: 4723
-}
+};
 
-driver = wd.remote(sauce)
+driver = wd.remote(sauce);
 
 driver.on('status', function(info) {
   console.log(info.cyan);
@@ -19,30 +22,48 @@ driver.on('command', function(meth, path, data) {
   console.log(' > ' + meth.yellow, path.grey, data || '');
 });
 
-curr = {foo: 1};
-handler = function(e, el){
-  if (e) { console.log('error', e) }
-  curr = el
-}
+current = {};
+
+handler = function(error, el){
+  if (error) {
+    console.log('error', error);
+  }
+  else if(typeof el === 'object'){
+    console.log("Returned in current");
+    current = el;
+  }
+  else {
+    console.log("Returned following string",el);
+  }
+
+};
 
 quit = function(){
   driver.quit(function(){
-    process.exit(1)
+    process.exit(1);
   });
-}
-
+};
+var profileKeys=Object.keys(profiles);
 console.log(' Profiles '.green);
-console.log('=========='.green)
-Object.keys(profiles).forEach(function(key){
+console.log('=========='.green);
+profileKeys.forEach(function(key){
   console.log(key);
 });
 
+var profile = function(){
+  telme("Profile").then(function(str){
+  return str;
+});
+};
 
-
-console.log('starting driver')
-var caps = profiles['iPad'];
-caps.newCommandTimeout = 0;
-driver.init(caps, function(){
-  console.log('driver started');
-  repl.start({prompt:'("o")', useGlobal: true})
+telme("Enter the profile you wish to start.").then(function(str){
+  return str;
+}).then(function(str){
+  var caps = profiles[str];
+  caps.newCommandTimeout = 0;
+  console.log("Caps + : " + caps);
+  driver.init(caps, function(){
+    console.log('driver started');
+    repl.start({prompt:'("o")', useGlobal: true});
+  });
 });
